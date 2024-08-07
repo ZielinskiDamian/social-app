887b81271827f0e247d28fb587b83b6d35b89743
damian zielinski <dazielinscy@gmail.com>
2024-08-07 16:48:07 +0200

    zakończona na likach

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,33 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Post from '../components/Post';
+
 const Home = () => {
-	return <h2>Home</h2>;
+	const [posts, setPosts] = useState([]);
+	const getLatestPosts = (res) => {
+		axios
+			.post('https://akademia108.pl/api/social-app/post/latest')
+			.then((res) => {
+				setPosts(res.data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	useEffect(() => {
+		getLatestPosts();
+	}, []);
+	console.log(posts);
+	return (
+		<div>
+			<h2>Home</h2>
+			<div className='postList'>
+				{posts.map((post) => {
+					return <Post post={post} key={post.id} />
+				})}
+			</div>
+		</div>
+	);
 };
 export default Home;
