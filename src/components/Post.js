8119b50ca11e2f608ea07211f3f1e0f350406331
damian zielinski <dazielinscy@gmail.com>
2024-08-09 13:12:17 +0200

    zmiany

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,7 @@ import './Post.css';
 import { useState } from 'react';
 
 const Post = (props) => {
-	const [likesCount, setLikesCount] = useState(props.post.likes.length)
+	const [likesCount, setLikesCount] = useState(props.post.likes.length);
 	return (
 		<div className='post'>
 			<div className='avatar'>
@@ -15,9 +15,9 @@ const Post = (props) => {
 				<div className='postMeta'>
 					<div className='author'>{props.post.user.username}</div>
 					<div className='date'>{props.post.created_at.substring(0, 10)}</div>
-					<div className='postContent'>{props.post.content}</div>
-					<div className='likes'>{likesCount}</div>
 				</div>
+				<div className='postContent'>{props.post.content}</div>
+				<div className='likes'>{likesCount}</div>
 			</div>
 		</div>
 	);
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,38 @@
+import { useState } from 'react';
 import './Login.css';
 const Login = () => {
-	return <h2>Login</h2>;
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	});
+
+	const handleInputChange = (e) => {
+		const target = e.target;
+		const name = target.name;
+
+		setFormData({ ...formData, [name]: target.value });
+	};
+
+	return (
+		<div className='login'>
+			<form>
+				<input
+					type='text'
+					name='username'
+					placeholder='Username'
+					value={formData.username}
+					onChange={handleInputChange}
+				></input>
+				<input
+					type='password'
+					name='password'
+					placeholder='Password'
+					value={formData.password}
+					onChange={handleInputChange}
+				></input>
+				<button className='btn'>Login </button>
+			</form>
+		</div>
+	);
 };
 export default Login;
